c6ed694441fe86f58086a0b8340047e351466507
unknown <chris.cornelis.lukito@gmail.com>
2026-05-05 08:07:21 +0700

    second commit

---
+++ MODUL10_103022400075/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace MODUL10_103022400075
+{
+    public class Game
+    {
+        public string nama {  get; set; }
+        public string developer { get; set; }
+        public int tahunRilis { get; set; }
+        public string genre { get; set; }
+        public double rating { get; set; }
+        public string[] platform { get; set; }
+        public string[] mode { get; set; }
+        public bool isOnline { get; set; }
+        public int harga { get; set; }
+
+        public Game() { }
+    }
+}
---
+++ MODUL10_103022400075/GameController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MODUL10_103022400075.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private static List<Game>_games = new List<Game>
+        {
+            new Game { nama = "Valorant", developer = "Riot Games", tahunRilis = 2020, genre = "FPS", rating = 8.5, platform = ["PC"], mode = ["Multiplayer"], isOnline = true, harga = 0},
+            new Game { nama = "GTA V", developer = "Rockstar Games", tahunRilis = 2013, genre = "Open World", rating = 9.5, platform = ["PC","PS4","PS5","Xbox"], mode = ["Singleplayer","Multiplayer"], isOnline = true, harga = 300000},
+            new Game { nama = "The Witcher 3", developer = "CD Project Red", tahunRilis = 2015, genre = "RPG", rating = 9.7, platform = ["PC","PS4","PS5","Xbox","Switch"], mode = ["Singleplayer"], isOnline = false, harga = 250000}
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Game>> Get()
+        {
+            return Ok(_games);
+        }
+
+        [HttpGet("{nama}")]
+        public ActionResult<Game> Get(string nama)
+        {
+            var game = _games.FirstOrDefault(f => f.nama == nama);
+            if (game == null) return NotFound();
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Game newGame)
+        {
+            _games.Add(newGame);
+            return Ok(newGame);
+        }
+        [HttpPut("{nama}")]
+        public ActionResult<Game> Put([FromBody] Game newGame, string nama)
+        {
+            var game = _games.FindIndex(f => f.nama == nama);
+            if (game == -1) return NotFound();
+            _games[game] = newGame;
+            return Ok(newGame);
+        }
+            
+
+
+
+        [HttpDelete("{nama}")]
+        public ActionResult Delete(string nama)
+        {
+            var game = _games.FirstOrDefault(f => f.nama == nama);
+            if (game == null) return NotFound();
+
+            _games.Remove(game);
+            return Ok();
+        }
+    }
+}
